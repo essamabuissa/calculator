03f7e2c73eab8ad954542b4f45f9bd76d05c9ea3
Essam <essamabuissa@hotmail.com>
2020-02-04 14:36:44 +0200

    Calculator code

--- calculator.py
+++ calculator.py
@@ -4,20 +4,20 @@ def main():
 	pass
 	first_number = (input("Enter first number: "))
 	second_number = (input("Enter second number: "))
+	operations = input("Choose the operation (+,-,/,*): ")
 	if first_number.isnumeric() == False or second_number.isnumeric() == False:
 		print("numbers are invalid")
-	operations = input("Choose the operation (+,-,/,*): ")
-	if operations != "*" and operations != "+" and operations != "-" and operations != "/":
-		print("operation is invalid")
-	if operations == "*":
-		print(float(first_number) * float(second_number))
-	elif operations == "-":
-		print(float(first_number) - float(second_number))
-	elif operations	== "+":
-		print(float(first_number) + float(second_number))
-	elif operations == "/":
-		print(float(first_number) / float(second_number))
-
+	else:
+		if operations == "*":
+			print("The answer is " + str(int(first_number) * int(second_number)))
+		elif operations == "+":
+			print("The answer is " + str(int(first_number) + int(second_number)))
+		elif operations == "-":
+			print("The answer is " + str(int(first_number) - int(second_number)))
+		elif operations == "/":
+			print("The answer is " + str(int(first_number) // int(second_number)))
+		else:
+			print("operation is invalid")
 
 
 
