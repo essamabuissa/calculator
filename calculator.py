78ee1a52597d70eba613fc8886a47a354651e595
Essam <essamabuissa@hotmail.com>
2020-02-04 13:41:37 +0200

    "Calculator Code"

--- calculator.py
+++ calculator.py
@@ -2,7 +2,22 @@
 def main():
 	#write your code here
 	pass
-	
+	first_number = (input("Enter first number: "))
+	second_number = (input("Enter second number: "))
+	if first_number.isnumeric() == False or second_number.isnumeric() == False:
+		print("numbers are invalid")
+	operations = input("Choose the operation (+,-,/,*): ")
+	if operations != "*" and operations != "+" and operations != "-" and operations != "/":
+		print("operation is invalid")
+	if operations == "*":
+		print(float(first_number) * float(second_number))
+	elif operations == "-":
+		print(float(first_number) - float(second_number))
+	elif operations	== "+":
+		print(float(first_number) + float(second_number))
+	elif operations == "/":
+		print(float(first_number) / float(second_number))
+
 
 
 
